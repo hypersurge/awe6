f00a60d56cd172539a65488efaf22424ab1f8eb8
robert.fell <robert.fell@hypersurge.com>
2011-02-25 23:03:17 +0000

    Streamlined AudioManager and re-concealed internal functionality

--- app/src/awe6/interfaces/IAudioManager.hx
+++ app/src/awe6/interfaces/IAudioManager.hx
@@ -41,29 +41,29 @@ interface IAudioManager
 	 * @param	?volume	Adjusts this sound's amplitude relative to the audioChannel.  0...1: 0 is silent, 1 is full.
 	 * @param	?pan	Adjusts this sound's stereo effect relative to the audioChannel.  -1...1: -1 is left channel only, 0 is central, 1 is right channel only.
 	 * @param	?isIgnoredIfPlaying	If true and this sound is already playing in the specified channel the start request will be skipped.  If false there is a potential for the same sound to play over the top of itself.
-	 * @return	The sound.
+	 * @param	?onCompleteCallback	Callback method to execute on sound complete.
 	 */
-	 function start( id:String, ?audioChannelType:EAudioChannel, ?loops:Int = 1, ?startTime:Int = 0, ?volume:Float = 1, ?pan:Float = 0, ?isIgnoredIfPlaying:Bool = false ):IAudio;
+	 function start( id:String, ?audioChannelType:EAudioChannel, ?loops:Int = 1, ?startTime:Int = 0, ?volume:Float = 1, ?pan:Float = 0, ?isIgnoredIfPlaying:Bool = false, ?onCompleteCallback:Void->Void ):Void;
 	/**
 	 * End playback of any specified sound.  To stop all sounds on all channels, leave all parameters blank.
-	 * @param	?id	The unique id of the audio media asset intended to be stopped.  If blank will stop all sounds on the specific audioChannel.
+	 * @param	?id	The unique id of the audio media asset intended to be stopped.  If null will stop all sounds on the specific audioChannel.
 	 * @param	?audioChannelType	If specified will only stop sounds assigned to this channel.
 	 */
-	function stop( ?id:String = "", ?audioChannelType:EAudioChannel ):Void;
+	function stop( ?id:String, ?audioChannelType:EAudioChannel ):Void;
 	/**
 	 * Adjusts the playback of any specified sound.  To adjust all sounds, ommit id and audioChannelType.
-	 * @param	?id	The unique id of the audio media asset intended to be transformed.  If blank will transform all sounds on the specific audioChannel.
+	 * @param	?id	The unique id of the audio media asset intended to be transformed.  If null will transform all sounds on the specific audioChannel.
 	 * @param	?audioChannelType	If specified will only transform sounds assigned to this channel.
 	 * @param	?volume	Adjusts this sound's amplitude relative to the audioChannel.  0...1: 0 is silent, 1 is full.
 	 * @param	?pan	Adjusts this sound's stereo effect relative to the audioChannel.  -1...1: -1 is left channel only, 0 is central, 1 is right channel only.
 	 * @param	?asRelative	If true will adjust sounds relative to their original transformation.  If false will set them as absolute values.
 	 */
-	function transform( ?id:String = "", ?audioChannelType:EAudioChannel, ?volume:Float = 1, ?pan:Float = 0, ?asRelative:Bool = false  ):Void;
+	function transform( ?id:String, ?audioChannelType:EAudioChannel, ?volume:Float = 1, ?pan:Float = 0, ?asRelative:Bool = false  ):Void;
 	/**
 	 * Discover if a specified sound is playing.
-	 * @param	?id	The unique id of the audio media asset under investigation.
+	 * @param	?id	The unique id of the audio media asset under investigation.  If null will search entire audioChannel for activity.
 	 * @param	?audioChannelType	If specified will only investigate the specified channel.  If ommitted will investigate all channels.
 	 * @return	Returns true if a match is found, otherwise false.
 	 */
-	function isPlaying( ?id:String = "", ?audioChannelType:EAudioChannel ):Bool;
+	function isPlaying( ?id:String, ?audioChannelType:EAudioChannel ):Bool;
 }
