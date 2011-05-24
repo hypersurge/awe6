aac60085c7b3cb579880ea4080dd9c28a71c1836
robert.fell <robert.fell@hypersurge.com>
2011-05-24 21:41:21 +0000

    Added generateIntrinsics.bat call to hxml files

--- app/scripts/assets.bat
+++ app/scripts/assets.bat
@@ -2,6 +2,14 @@
 
 if not exist ..\src\assets call generateIntrinsics.bat
 
+:: FOR /F %%i IN ('DIR /B /O:D 
+:: "..\src\assets\_readme.txt" 
+:: "..\assets\audio.swf"
+:: "..\assets\gui.swf"
+:: "..\assets\preloader.swf"
+:: ') DO SET NEWEST=%%i
+:: IF NOT "%NEWEST%" == "..\src\assets\_readme.txt" call generateIntrinsics.bat
+
 xcopy ..\assets\_readme.txt ..\bin\* /Q /Y
 xcopy ..\assets\audio.swf ..\bin\* /Q /Y
 xcopy ..\assets\config.xml ..\bin\* /Q /Y
