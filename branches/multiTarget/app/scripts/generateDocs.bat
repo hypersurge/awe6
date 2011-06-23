11aaf618ae892e7441a0eee0b17324f5284bdb3f
robert.fell@hypersurge.com <robert.fell@hypersurge.com@74770ea5-1ac6-b23e-f6a5-23107cab234e>
2011-06-23 21:16:07 +0000

    Updating multiTarget branch

---
+++ branches/multiTarget/app/scripts/generateDocs.bat
@@ -0,0 +1,12 @@
+:: This utlity requires chxdoc (http://code.google.com/p/caffeine-hx/wiki/ChxDoc) which is provided in Windows compiled format for convenience
+
+cd ..
+rmdir docs /s /q
+haxe -cp src -swf9 bin/awe6.swf -swf-version 10 -xml scripts/chxdoc/output.xml --no-output awe6.Types
+cd scripts/chxdoc
+chxdoc -o ../../docs --tmpDir=_tmp --installTemplate=true --templateDir=template --generateTodoFile=true --showTodoTags=true --includeOnly=awe6.core.,awe6.interfaces.,Array,Bool,Class,Enum,Float,Hash,Int,List,Dynamic,String,Type,UInt,Void --title="awe6" --subtitle="Inverted Game Framework" --ignoreRoot=false output.xml
+rmdir _tmp /s /q
+del output.xml /q /f
+cd ..
+start ../docs/index.html
+
---
+++ branches/multiTarget/app/src/awe6/interfaces/EKey.hx
@@ -0,0 +1,142 @@
+/*
+ *                        _____ 
+ *     _____      _____  / ___/
+ *    /__   | /| /   _ \/ __ \ 
+ *   / _  / |/ |/ /  __  /_/ / 
+ *   \___/|__/|__/\___/\____/  
+ *    awe6 is game, inverted
+ * 
+ * Copyright (c) 2010, Robert Fell, awe6.org
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package awe6.interfaces;
+
+/**
+ * Representing the keys on a Keyboard for use in the IInputKeyboard virtual controller.
+ * <p>Can be extended with SubType by using concrete project values.</p> 
+ * @author	Robert Fell
+ */
+enum EKey 
+{
+	NUM_LOCK; 
+	CLEAR;
+	HELP;
+	ALT;
+	BACKSPACE;
+	CAPS_LOCK;
+	CONTROL;
+	DELETE;
+	DOWN;
+	END;
+	ENTER;
+	ESCAPE;
+	F1;
+	F10;
+	F11;
+	F12;
+	F13;
+	F14;
+	F15;
+	F2;
+	F3;
+	F4;
+	F5;
+	F6;
+	F7;
+	F8;
+	F9;
+	HOME;
+	INSERT;
+	LEFT;
+	NUMPAD_0;
+	NUMPAD_1;
+	NUMPAD_2;
+	NUMPAD_3;
+	NUMPAD_4;
+	NUMPAD_5;
+	NUMPAD_6;
+	NUMPAD_7;
+	NUMPAD_8;
+	NUMPAD_9;
+	NUMPAD_ADD;
+	NUMPAD_DECIMAL;
+	NUMPAD_DIVIDE;
+	NUMPAD_ENTER;
+	NUMPAD_MULTIPLY;
+	NUMPAD_SUBTRACT;
+	PAGE_DOWN;
+	PAGE_UP;
+	RIGHT;
+	SHIFT;
+	SPACE;
+	TAB;
+	UP;
+	A;
+	B;
+	C;
+	D;
+	E;
+	F;
+	G;
+	H;
+	I;
+	J;
+	K;
+	L;
+	M;
+	N;
+	O;
+	P;
+	Q;
+	R;
+	S;
+	T;
+	U;
+	V;
+	W;
+	X;
+	Y;
+	Z;
+	NUMBER_0;
+	NUMBER_1;
+	NUMBER_2;
+	NUMBER_3;
+	NUMBER_4;
+	NUMBER_5;
+	NUMBER_6;
+	NUMBER_7;
+	NUMBER_8;
+	NUMBER_9;
+	COLON;
+	EQUALS;
+	HYPHEN;
+	SLASH;
+	TILDE;
+	SQUARELEFT;
+	SQUARERIGHT;
+	BACKSLASH;
+	APOSTROPHE;
+	TOPLEFT;
+	/**
+	 * Allows EKey to be extended (e.g. for using non US / English keyboards).
+	 */
+	SUB_TYPE( value:Dynamic );
+}
