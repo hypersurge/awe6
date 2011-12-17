df0f82ad2dc1fd4261a7c75595629de364edf048
robert.fell <robert.fell@hypersurge.com>
2011-12-17 19:37:47 +0000

    Attempting onload event for iframe in ie9

--- docs/demos/html5/awe6.js
+++ docs/demos/html5/awe6.js
@@ -14797,3 +14797,4 @@ jeash.display.Graphics.BLEND_SHADER = 14;
 jeash.display.Graphics.JEASH_SIZING_WARM_UP = 10;
 jeash.display.Graphics.JEASH_MAX_DIMENSION = 5000;
 window.addEventListener( "load", ApplicationMain.main, false );
+alert( typeof gadgets.util.registerOnLoadHandler );
