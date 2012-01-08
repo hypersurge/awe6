f81419e6f923c947e740afe74228e30795ff61d0
mihail12@gmail.com <mihail12@gmail.com@74770ea5-1ac6-b23e-f6a5-23107cab234e>
2012-01-08 22:36:57 +0000

    Small changes on AKernel -- reorganization of logic.

--- branches/multiDrivers/app/src/awe6/core/drivers/AKernel.hx
+++ branches/multiDrivers/app/src/awe6/core/drivers/AKernel.hx
@@ -1,23 +1,23 @@
 /*
- *                        _____ 
+ *                        _____
  *     _____      _____  / ___/
- *    /__   | /| /   _ \/ __ \ 
- *   / _  / |/ |/ /  __  /_/ / 
- *   \___/|__/|__/\___/\____/  
+ *    /__   | /| /   _ \/ __ \
+ *   / _  / |/ |/ /  __  /_/ /
+ *   \___/|__/|__/\___/\____/
  *    awe6 is game, inverted
- * 
+ *
  * Copyright (c) 2010, Robert Fell, awe6.org
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -104,31 +104,43 @@ class AKernel extends Process, implements IKernel
 		// FIXME: (M. Ivanchev) generally factory, tools and _view will always
 		// be null at this point, except if a subclass sets them first?
 		// R. Fell -- yes, these null checks are to allow easier subclassing, probably overkill?
-		if ( factory == null )
-		{
-			factory = p_factory;
-		}
+		// M. Ivanchev -- OK, definetely not needed because:
+		//
+		// * A subclass can simply pass the custom factory and context through p_factory, p_context
+		// * There are not much other options for Tools to be honest and this is a fully internal mechanism.
+		// * There are not much other options for the initialization of the main view.
+		//
+		// And besides all that: since the philosophy of a constructor is to just assign the injected values,
+		// I am moving the tools and the _view to _init.
+		//
+		// If you agree on that, I'll remove the FIXME.
+		//
+		factory = p_factory;
 		_context = p_context;
-		if ( tools == null )
-		{
-			tools = _tools = new Tools( this );
-		}
-		if ( _view == null )
-		{
-			_view = new View( this, _context, 0, this );
-		}
 		super( this );
 	}
-	
+
 	override private function _init():Void
 	{
 		super._init();
-		// R. Fell -- these 2 bools should be set here because they can influence construction of Managers.
-		isDebug = factory.isDebug;
-		isLocal = _driverGetIsLocal();
-		_isPreloaded = false;
+		tools = _tools = new Tools( this );
+		_view = new View( this, _context, 0, this );
 		_processes = new List<IProcess>();
 		_helperFramerate = new _HelperFramerate( factory.targetFramerate );
+		_isPreloaded = false;
+
+		// Perform driver specific initializations.
+		//
+
+		_driverInit();
+		isDebug = factory.isDebug;
+		isLocal = _driverGetIsLocal();
+		isEyeCandy = true;
+		isFullScreen = false;
+
+		// Initialize managers.
+		//
+		
 		assets = _assetManagerProcess = new AAssetManager( _kernel );
 		audio =	_audioManager = new AudioManager( _kernel );
 		inputs = _inputManager = new InputManager( _kernel );
@@ -140,12 +152,12 @@ class AKernel extends Process, implements IKernel
 		_addProcess( _inputManager );
 		_addProcess( _sceneManager );
 		_addProcess( _messageManager );
-		_driverInit();
-		isEyeCandy = true;
-		isFullScreen = false;
-		// M. Ivanchev -- this seems to be right place for signaling complete
-		// initialization to the factory.
+
+		// Signal completion to the factory and initialize factory-dependent components.
+		//
+
 		factory.onInitComplete( this );
+
 		session = factory.createSession();
 		session.reset();
 		_preloader = factory.createPreloader();
