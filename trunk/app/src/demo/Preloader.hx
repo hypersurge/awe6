c807be307cc2daa1908b047585a7a5b2c37ce69b
enzogupi <enzogupi@74770ea5-1ac6-b23e-f6a5-23107cab234e>
2012-09-29 20:51:28 +0000

    Removed Preloader comments.

--- trunk/app/src/demo/Preloader.hx
+++ trunk/app/src/demo/Preloader.hx
@@ -31,26 +31,6 @@ package demo;
 import awe6.core.APreloader;
 
 class Preloader extends APreloader {
-
-/*
-	private var _preloaderMovieClip:PreloaderMovieClip;
-
-	override private function _init():Void {
-		super._init();
-		_preloaderMovieClip = new PreloaderMovieClip();
-		_preloaderMovieClip.progress.stop();
-		_preloaderMovieClip.x = ( _kernel.factory.width - _preloaderMovieClip.width ) / 2;
-		_preloaderMovieClip.y = ( _kernel.factory.height - _preloaderMovieClip.height ) / 2;
-		_context.addChild( _preloaderMovieClip );
-	}
 	
-	override private function _updater( ?p_deltaTime:Int = 0 ):Void {
-		super._updater( p_deltaTime );
-		if ( _preloaderMovieClip != null ) {
-			_preloaderMovieClip.progress.gotoAndStop( Std.int( 100 * progress ) );
-		}
-	}
-*/
-
 }
 
