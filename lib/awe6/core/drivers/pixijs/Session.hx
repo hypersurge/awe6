024abe5627c7a2dc4c1932a782856b61a9d59642
Robert Fell <robert.fell@hypersurge.com>
2017-11-05 11:34:17 -0800

    pixijs Session

--- lib/awe6/core/drivers/pixijs/Session.hx
+++ lib/awe6/core/drivers/pixijs/Session.hx
@@ -29,13 +29,69 @@
 
 package awe6.core.drivers.pixijs;
 import awe6.core.drivers.ASession;
+import js.Browser;
+import js.html.Storage;
 
 /**
- * This class provides an easy driver package to remap from.
- * To use it add compiler conditional: -D awe6DriverRemap
- * Or, you may prefer to create your driver in a new namespace, and add a new directive for its mapping.
+ * This Session class provides PixiJS target overrides.
  * @author	Robert Fell
  */
 class Session extends ASession
 {
+	private static inline var _CONFIG_SESSION_REMOVED = "settings.sessionSaved"; // true or false, defaults to true
+	
+	private var _storage:Storage;
+	
+	override private function _init() 
+	{
+		var l_isSessionSaved:Bool = true;
+		if ( _kernel.getConfig( _CONFIG_SESSION_REMOVED ) != null )
+		{
+			l_isSessionSaved = _kernel.getConfig( _CONFIG_SESSION_REMOVED ) != "false";
+		}
+		_storage = l_isSessionSaved ? Browser.getLocalStorage() : Browser.getSessionStorage();
+		super._init();
+	}
+	
+	override private function _driverLoad():Void
+	{
+		_savedData = { };
+		if ( _storage != null )
+		{
+			try
+			{
+				var l_item:String = _storage.getItem( _kernel.factory.id );
+				if ( l_item != null )
+				{
+					_savedData = _tools.unserialize( l_item );
+				}
+			}
+			catch ( p_error:Dynamic ) {} // sometimes browsers behave badly!
+		}
+	}
+	
+	override private function _driverReset():Void
+	{
+		if ( _storage != null )
+		{
+			try
+			{
+				_storage.removeItem( _kernel.factory.id );
+			}
+			catch ( p_error:Dynamic ) {} // sometimes browsers behave badly!
+		}
+		_savedData = {};
+	}
+	
+	override private function _driverSave():Void
+	{
+		if ( _storage != null )
+		{
+			try
+			{
+				_storage.setItem( _kernel.factory.id, _tools.serialize( _savedData ) );
+			}
+			catch ( p_error:Dynamic ) {} // sometimes browsers behave badly!
+		}
+	}
 }
