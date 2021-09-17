e1299b200141f71ad01011626f1a89875124d93b
Robert Fell <rob@hypersurge.com>
2021-09-17 10:43:09 -0700

    pixijs driver sceneTransition workaround for Pixi v6 generateTexture options overrides

--- lib/awe6/core/drivers/pixijs/SceneTransition.hx
+++ lib/awe6/core/drivers/pixijs/SceneTransition.hx
@@ -1,23 +1,23 @@
 /*
- *                        _____ 
+ *                        _____
  *     _____      _____  / ___/
- *    /__   | /| /   _ \/ __ \ 
- *   / _  / |/ |/ /  __  /_/ / 
- *   \___/|__/|__/\___/\____/  
+ *    /__   | /| /   _ \/ __ \
+ *   / _  / |/ |/ /  __  /_/ /
+ *   \___/|__/|__/\___/\____/
  *    awe6 is game, inverted
- * 
+ *
  * Copyright (c) 2010, Robert Fell, awe6.org
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,11 +29,12 @@
 
 package awe6.core.drivers.pixijs;
 import awe6.core.drivers.ASceneTransition;
-import pixi.core.Pixi.RendererType;
+import pixi.core.Pixi;
 import pixi.core.Pixi.ScaleModes;
 import pixi.core.display.DisplayObject;
 import pixi.core.math.shapes.Rectangle;
 import pixi.core.sprites.Sprite;
+import pixi.core.textures.RenderTexture;
 #if (pixijs >= "5.0.0")
 private typedef AbstractRenderer = pixi.core.renderers.AbstractRenderer;
 #else
@@ -46,23 +47,37 @@ private typedef AbstractRenderer = pixi.core.renderers.SystemRenderer;
  */
 class SceneTransition extends ASceneTransition
 {
-	override private function _init():Void 
+	override private function _init():Void
 	{
 		super._init();
-		var l_renderer:AbstractRenderer = untyped _kernel._renderer;
-		var l_displayObject:DisplayObject = untyped _kernel.scenes.scene.view.context;
-		var l_bounds = l_displayObject.getBounds();
-		if ( ( l_renderer != null ) && ( l_displayObject != null ) )
+		try
 		{
-			var l_rectangle = new Rectangle( 0, 0, _kernel.factory.width, _kernel.factory.height );
-			var l_texture = l_renderer.generateTexture( l_displayObject, ScaleModes.DEFAULT, 1, l_rectangle );
-			var l_sprite:Sprite = new Sprite( l_texture );
-			_context.interactive = false;
-			_context.addChild( l_sprite );
+			var l_renderer:AbstractRenderer = untyped _kernel._renderer;
+			var l_displayObject:DisplayObject = untyped _kernel.scenes.scene.view.context;
+			var l_bounds = l_displayObject.getBounds();
+			if ( ( l_renderer != null ) && ( l_displayObject != null ) )
+			{
+				var l_rectangle = new Rectangle( 0, 0, _kernel.factory.width, _kernel.factory.height );
+				var l_texture:RenderTexture;
+				if ( Std.parseInt( Pixi.VERSION.substr( 0, 1 ) ) >= 6 )
+				{
+					l_texture = l_renderer.generateTexture( l_displayObject, untyped {scaleMode:ScaleModes.DEFAULT, resolution:1, region:l_rectangle}, 1, l_rectangle );
+				}
+				else
+				{
+					l_texture = l_renderer.generateTexture( l_displayObject, ScaleModes.DEFAULT, 1, l_rectangle );
+				}
+				var l_sprite:Sprite = new Sprite( l_texture );
+				_context.interactive = false;
+				_context.addChild( l_sprite );
+			}
+		}
+		catch ( p_error:Dynamic )
+		{
 		}
 	}
-	
-	override private function _updater( p_deltaTime:Int = 0 ):Void 
+
+	override private function _updater( p_deltaTime:Int = 0 ):Void
 	{
 		super._updater( p_deltaTime );
 		if ( !isDisposed )
@@ -71,4 +86,3 @@ class SceneTransition extends ASceneTransition
 		}
 	}
 }
-
